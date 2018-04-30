2906e8dbba892d967ae60e52d062d09031e0ae8b
Tiago Paiva <tiagoapolo@gmail.com>
2018-04-30 15:53:39 -0300

    time correction

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var app = express();
 
+
+app.get('/', function(req, res){
+    res.redirect('/timestamp')
+});
+
 app.get('/timestamp', function(req, res){
-    res.send(Date.parse(new Date()))
+    res.send(Date.parse(new Date()).toString())
 });
 
 app.get('/datetime', function(req, res){
-    res.send(Date())
+    res.send(Date().toString())
 });
 
 app.listen(8081);
