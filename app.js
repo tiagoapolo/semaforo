6c909bdaae2e73c1dc569d94d519a74af569cdbd
Tiago Paiva <tiagoapolo@gmail.com>
2018-04-30 16:22:33 -0300

    time correction

--- app.js
+++ app.js
@@ -9,22 +9,16 @@ app.get('/', function(req, res){
 app.get('/timestamp', function(req, res){
     // Date.parse(new Date()).toString()
 
-    var localTime = new Date(); //get your local time
-    var utcTime = localTime.getUTCHours(); // find UTC hours
-    var estTime = new Date(); // create a new date object for the EST time
-    estTime.setHours(utcTime)
 
-    res.send(new Date.parse(estTime).toString())
+    res.send( Date.parse(new Date()).toString())
     
 });
 
 app.get('/datetime', function(req, res){
    
-
-    let options = { timeZone: "America/Sao_Paulo"}, // you have to know that New York in EST
     estTime = new Date();
 
-    res.send(estTime.toLocaleString("pt-BR", options))
+    res.send(estTime.toLocaleString("pt-BR"))
 
 
 });
